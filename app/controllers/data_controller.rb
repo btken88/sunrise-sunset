bfa967fb769f33267eaffb19c5e995a5af179963
Bryce Kennedy <btken88@gmail.com>
2020-06-09 10:55:48 -0600

    Create data action controllers. Fix before action in photos controller

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,4 +1,48 @@
 # frozen_string_literal: true
 
 class DataController < ApplicationController
+  before_action :find_datum_by_id, only: %i[show update destroy]
+  def index
+    @data = Datum.all
+    render json: @data
+  end
+
+  def show
+    render json: @datum
+  end
+
+  def create
+    @datum = Datum.create(
+      latitude: params[:latitude],
+      longitude: params[:longitude],
+      date: params[:date],
+      sunrise: params[:sunrise],
+      sunset: params[:sunset],
+      photo_id: params[:photo_id],
+      user_id: params[:user_id]
+    )
+    render json: @datum
+  end
+
+  def update
+    @datum.update(
+      latitude: params[:latitude],
+      longitude: params[:longitude],
+      date: params[:date],
+      sunrise: params[:sunrise],
+      sunset: params[:sunset],
+      photo_id: params[:photo_id],
+      user_id: params[:user_id]
+    )
+    render json: @datum
+  end
+
+  def destroy
+    @datum.destroy
+    render status: 204
+  end
+
+  def find_datum_by_id
+    @datum = Datum.find(params[:id])
+  end
 end
