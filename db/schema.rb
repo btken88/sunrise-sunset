0e45b8976a4798453821a6d8be04da6a3243c3c0
github username <github email address>
2020-06-09 11:27:10 -0600

    Change user class to have username and update controller.

--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,7 @@ ActiveRecord::Schema.define(version: 2020_06_09_163504) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
